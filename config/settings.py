083021b09d05bf0be47ee29b1522dc733fab9778
SpideyNotUnder <setora087@gmail.com>
2024-09-22 19:42:42 +0500

    the end

--- config/settings.py
+++ config/settings.py
@@ -134,6 +134,9 @@ LOCALE_PATHS = [
 
 STATIC_URL = '/static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'files/static'), ]
 
 # Default primary key field type
